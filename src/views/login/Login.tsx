53d517bbf4dd26ee60a9997d92c930b88f761165
zhangzhen <zhangzhen8@xiaomi.com>
2023-02-28 16:09:29 +0800

    hooks redux使用

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -7,6 +7,8 @@ import { mobilephone_regx } from '../../config/config';
 import API from '../../services/api';
 import http from '../../services/axios';
 import * as Hmall from '../../utils/util';
+import { RootDispatch, RootState } from '@src/store';
+import { useSelector, useDispatch } from 'react-redux';
 
 /**
  * 路由参数 Props 类型声明
@@ -14,14 +16,27 @@ import * as Hmall from '../../utils/util';
 interface RouterProps extends RouteComponentProps<any> {}
 
 const Login = (props: RouterProps) => {
+    const dispatch: RootDispatch = useDispatch();
+    const { login } = dispatch;
 
-    useEffect(() => {
-
-    }, []);
+    const count = useSelector((state: RootState) => {
+        const {
+            login: { count },
+        } = state;
+        return count;
+    });
 
     return (
         <div className={styles.login}>
-            
+            <p
+                className={styles.btn}
+                onClick={() => {
+                    login.INCREMENT();
+                }}
+            >
+                Add Btn
+            </p>
+            <p>count : {count}</p>
         </div>
     );
 };
